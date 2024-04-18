7535a1da3a0dd90763b1effd0b04b38d61b642e5
alex <alexmengyuan2022@gmail.com>
2024-04-18 14:12:16 +0800

    add log

--- pkg/im/group_qualification.go
+++ pkg/im/group_qualification.go
@@ -179,7 +179,7 @@ func (im *Manager) DeleteGroupQualification(groupQualification *GroupQualificati
 // check if group qualification exists, input is group id and address
 func (im *Manager) GroupQualificationExists(groupId [GroupIdLen]byte, address string, logger *logger.Logger) (bool, error) {
 	// log group qualification exists
-	logger.Infof("GroupQualificationExists groupId %s, address %s", iotago.EncodeHex(groupId[:]), address)
+	//logger.Infof("GroupQualificationExists groupId %s, address %s", iotago.EncodeHex(groupId[:]), address)
 	addressHash := Sha256Hash(address)
 	var addressHash32 [Sha256HashLen]byte
 	copy(addressHash32[:], addressHash)
