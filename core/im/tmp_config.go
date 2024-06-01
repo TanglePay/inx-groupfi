e2722d712f30b76178f20750b228c2b2f6cbc8f1
alex <alexmengyuan2022@gmail.com>
2024-06-01 22:54:51 +0800

    adjust token group token

--- core/im/tmp_config.go
+++ core/im/tmp_config.go
@@ -19,7 +19,7 @@ var (
 		    {"groupName":"dapper-groupfi","schemaVersion":1,"messageType":1,"authScheme":2, "qualifyType":"nft", "chainId":0,"collectionId":"0x1fd2407145ef147e0b06f835fef0e2059e56899aa8ce80147506893f837ea606"},
 			{"groupName":"Announcement","schemaVersion":1,"messageType":1,"authScheme":2, "qualifyType":"nft", "chainId":148,"collectionId":"0x544F353C02363D848dBAC8Dc3a818B36B7f9355e"},
 			{"groupName":"EtherVisions","schemaVersion":1,"messageType":1,"authScheme":2, "qualifyType":"nft", "chainId":148,"collectionId":"0xB85bdaf5eFf3f4c84d565923Eb1D62717dE17297"},
-			{"groupName":"TOKEN","schemaVersion":1,"messageType":1,"authScheme":2, "qualifyType":"token", "chainId":148,"collectionId":"","tokenThres":"1","tokenId":"0xfDbc4c5b14A538Aa2F6cD736b525C8e9532C5FA6"},
+			{"groupName":"TOKEN","schemaVersion":1,"messageType":1,"authScheme":2, "qualifyType":"token", "chainId":148,"collectionId":"","tokenThres":"1","tokenId":"0xA62e723Db40Db2695F2a4BBD9FbB6A858C8E8B23"},
 			{"groupName":"GroupFi Announcement","schemaVersion":1,"messageType":1,"authScheme":2, "qualifyType":"nft", "chainId":0,"collectionId":"0x115a9fdca6a2110cc3e4cc7f92555101c3d80addf56984003e51e004ac4f9148"},
 			{"groupName":"alpha-test","schemaVersion":1,"messageType":1,"authScheme":2, "qualifyType":"nft", "chainId":0,"collectionId":"0x23e5f8500132f9dfa8698e3d352f0d57bd79cf57533f273e87e31b6cd0e0a5ef"},
 		   	{"groupName":"iceberg-1","schemaVersion":1,"messageType":1,"authScheme":2, "qualifyType":"nft", "chainId":0,"collectionId":"0x064d0eaefb86a94eb326ff633c22cdf744decca954bb93b1572b449d324ae717"},
