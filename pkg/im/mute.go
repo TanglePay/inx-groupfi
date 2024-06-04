6776ae746a9f1937bbdf6f09fa1f4b4d4d212fe2
alex <alexmengyuan2022@gmail.com>
2024-06-04 14:57:15 +0800

    1

--- pkg/im/mute.go
+++ pkg/im/mute.go
@@ -242,7 +242,8 @@ func (im *Manager) CalculateReputationScore(groupId [GroupIdLen]byte, likedOrMut
 	tmp11 := float64(count * 1.0)
 	tmp2 := tmp1 * tmp11
 	// Perform the division and the rest of the calculation
-	reputationScore := 100.0 + float32(tmp2)
+	reputationScore := 100.0 + tmp2
+	reputationScore = math.Round(reputationScore*10) / 10
 
 	// log reputation score, likedTimes, mutedTimes, groupMemberCount, adderss，denominator,tmp1,tmp11,tmp2
 	Logger.Infof("CalculateReputationScore: reputationScore=%f, likedTimes=%d, mutedTimes=%d, groupMemberCount=%d, adderss=%s, denominator=%f, tmp1=%f, tmp11=%f, tmp2=%f",
