55821026d59aac4bf175d51019b98e197c568924
alex <alexmengyuan2022@gmail.com>
2024-06-30 21:43:16 +0800

    add iotaevm

--- core/im/routes.go
+++ core/im/routes.go
@@ -31,7 +31,6 @@ var evmNode = map[int]ChainConfig{
 		MaxFilterBlock: 999,
 		Contract:       "0xc2F820BA3DBe0755deE2cD0ddc8Bf2fEc1e57255",
 	},
-	// 8822 https://json-rpc.evm.iotaledger.net or wss://ws.json-rpc.evm.iotaledger.net
 	8822: {
 		RPC:            "https://json-rpc.evm.iotaledger.net",
 		WSS:            "wss://ws.json-rpc.evm.iotaledger.net",
