3593c0ae827ad06c841bd9fe59b6960db316c2ce
alex <alexmengyuan2022@gmail.com>
2024-07-30 20:45:28 +0800

    adjust back

--- core/im/init_helper.go
+++ core/im/init_helper.go
@@ -36,7 +36,7 @@ type OutputWithId struct {
 
 // handleGenericInit function will maintain a mark for is finished,
 // iterate all output under certain filter,
-func HandleGenericInit(initCtx *InitContext,
+func HandleGenericInitv2(initCtx *InitContext,
 	topic string,
 	outputIdsFetcher OutputIdsFetcher,
 	outputProcessors []OutputProcessor) {
@@ -212,7 +212,7 @@ var NftOutputIdsByTagFetcher = func(tag string) OutputIdsFetcher {
 	}
 }
 
-func HandleGenericInitv2(initCtx *InitContext,
+func HandleGenericInit(initCtx *InitContext,
 	topic string,
 	outputIdsFetcher OutputIdsFetcher,
 	outputProcessors []OutputProcessor) {
