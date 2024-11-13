ae7db47b09abc7d2d2da9b8126521a9bd5911be3
alex <alexmengyuan2022@gmail.com>
2024-11-13 16:19:50 +0800

    add lru back

--- core/im/component.go
+++ core/im/component.go
@@ -304,6 +304,7 @@ func run() error {
 		}
 		im.CurrentNodeProtocol = &resp.Protocol
 		im.ListeningCtx = ctx
+		im.InitializeOutputCache(1000)
 		indexerClient, err := nodeHTTPAPIClient.Indexer(ctx)
 		if err != nil {
 			CoreComponent.LogPanicf("failed to start worker: %s", err)
